b4429c2b8a1f3cae4ecc728d9324c321a33e14fa
praveenearle <praveen.earle@gmail.com>
2016-09-24 00:11:16 +0530

    angular js filters assignment 1
    angular js filters assignment 1

---
+++ angular js filters assgn1/angular js s1a2/scripts/script.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="angular.min.js" />
+
+
+var myApp = angular.module("myModule", []);
+
+myApp.controller("myController", function ($scope) {
+
+    $scope.firstname = "firstname";
+    $scope.lastname = "lastname";
+
+    
+
+
+        
+    }); 
+
